4ffdbf0b6aa1bf2ee5d248a4d818bc88ea68ff92
cj__ <cj__@7d8b11ba-8c55-0410-bd75-95494264abfd>
2007-07-06 02:23:12 +0000

    1.0.4 release

---
+++ tags/madcow-1.0.4/modules/memebot.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+# Watch URLs in channel, punish people for living under a rock
+
+import sys
+import re
+import os
+import urlparse
+import datetime
+from pysqlite2 import dbapi2 as sqlite
+from sqlobject import *
+import random
+
+# object model
+class url(SQLObject):
+	url = StringCol(alternateID = True)
+	clean = StringCol(alternateID = True)
+	author = ForeignKey('author')
+	channel = ForeignKey('channel')
+	citations = IntCol(default = 0)
+	posted = DateTimeCol(default = datetime.datetime.now)
+
+	def truncated_url(self):
+		if (len(self.url) > 48):
+			return self.url[:48] + ' ... ' + self.url[-4:]
+		else:
+			return self.url
+
+	turl = property(truncated_url)
+
+class author(SQLObject):
+	name = StringCol(alternateID = True)
+	urls = MultipleJoin('url')
+	pointsNew = IntCol(default = 0)
+	pointsOld = IntCol(default = 0)
+	pointsCredit = IntCol(default = 0)
+
+class channel(SQLObject):
+	name = StringCol(alternateID = True)
+	urls = MultipleJoin('url')
+
+
+# class for this module
+class match(object):
+	def __init__(self, ns='default', config=None, dir=None):
+		self.enabled = True				# True/False - enabled?
+		self.pattern = re.compile('(.+)')	# regular expression that needs to be matched
+		self.requireAddressing = False			# True/False - require addressing?
+		self.thread = False				# True/False - should bot spawn thread?
+		self.wrap = False				# True/False - wrap output?
+		self.help = 'score - get memescore'
+
+		if dir is None: dir = os.path.abspath(os.path.dirname(sys.argv[0]) + '/..')
+		file = dir + '/db-%s-memes' % ns
+
+		self.matchURL = re.compile('(http://\S+)', re.I)
+
+		sqlhub.processConnection = connectionForURI('sqlite://' + file)
+		url.createTable(ifNotExists = True)
+		author.createTable(ifNotExists = True)
+		channel.createTable(ifNotExists = True)
+
+		self.riffs = [
+			'OLD MEME ALERT!',
+			'omg, SO OLD!',
+			'Welcome to yesterday.',
+			'been there, done that.',
+			'you missed the mememobile.',
+			'oldest. meme. EVAR.',
+			'jesus christ you suck.',
+			'you need a new memesource, bucko.',
+			'that was funny the first time i saw it.',
+		]
+
+
+	def cleanURL(self, url):
+		uri = list(urlparse.urlparse(url))
+		uri[1] = uri[1].lower()
+		if uri[2] == '': uri[2] = '/'
+		uri[5] = ''
+		return urlparse.urlunparse(uri)
+
+	# XXX magic numbers, move to config
+	def getScoreForAuthor(self, a):
+		return	a.pointsNew    *  1 + \
+			a.pointsOld    * -2 + \
+			a.pointsCredit *  2
+
+	def top10(self):
+		scores = [(a.name, self.getScoreForAuthor(a)) for a in author.select()]
+		return sorted(scores, lambda x, y: cmp(y[1], x[1]))[:10]
+
+
+	# function to generate a response
+	def response(self, *args, **kwargs):
+		nick = kwargs['nick'].lower()
+		chan = kwargs['channel']
+		addressed = kwargs['addressed']
+		message = kwargs['args'][0]
+
+		if addressed is True and 'score' in message:
+			scores = []
+			for i, data in enumerate(self.top10()):
+				name, score = data
+				scores.append('#%s: %s (%s)' % (i + 1, name, score))
+			return ', '.join(scores)
+			
+
+
+		match = self.matchURL.search(message)
+		if match is None: return
+		orig = match.group(1)
+		clean = self.cleanURL(orig)
+
+		try: me = author.byName(nick)
+		except SQLObjectNotFound: me = author(name = nick)
+
+		try:
+			# old meme
+			old = url.byClean(clean)
+
+			# chew them out unless its my own
+			if old.author.name != nick:
+				response = 'first posted by %s on %s' % (old.author.name, old.posted)
+				riff = random.choice(self.riffs)
+				old.author.pointsCredit = old.author.pointsCredit + 1
+				me.pointsOld = me.pointsOld + 1
+				old.citations = old.citations + 1
+				return '%s %s' % (riff, response)
+
+
+		except SQLObjectNotFound:
+			try: c = channel.byName(chan)
+			except SQLObjectNotFound: c = channel(name = chan)
+
+			url(url = orig, clean = clean, author = me, channel = c)
+
+			me.pointsNew = me.pointsNew + 1
+
+		except Exception, e:
+			print >> sys.stderr, 'error: %s' % e
+
+		return
+
+
+
+# this is just here so we can test the module from the commandline
+def main(argv = None):
+	if argv is None: argv = sys.argv[1:]
+	obj = match()
+	print obj.response(nick='testUser', channel='#test', args=[argv[0]], addressed=True)
+
+	return 0
+
+if __name__ == '__main__': sys.exit(main())
