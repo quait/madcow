e0dc43da367b85d9befdf89d0d0b3f3a8404db78
cj__ <cj__@7d8b11ba-8c55-0410-bd75-95494264abfd>
2007-07-25 04:13:22 +0000

    release 1.0.8

---
+++ tags/madcow-1.0.8/madcow.py
@@ -0,0 +1,300 @@
+#!/usr/bin/env python
+
+__version__ = '1.0.8'
+__author__ = 'Christopher Jones <cjones@gruntle.org>'
+__copyright__ = """
+Copyright (C) 2007 Christopher Jones <cjones@gruntle.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+"""
+
+import sys
+import os
+from ConfigParser import ConfigParser
+from optparse import OptionParser
+import re
+import threading
+import time
+import logging
+from logging import DEBUG, INFO, WARN, ERROR, CRITICAL
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s')
+
+class Request(object):
+	def __init__(self, message=None):
+		self.message = message
+
+		"""
+		these are some attributes that various
+		plugins and modules may expect to be set, so
+		give them defaults
+		"""
+
+		self.nick = None
+		self.addressed = False
+		self.correction = False
+		self.channel = None
+		self.args = []
+
+	def __getattr__(self, attr):
+		return None
+
+	def getDict(self):
+		return self.__dict__
+
+	params = property(getDict)
+
+class Madcow(object):
+	def __init__(self, config=None, dir=None):
+		self.config = config
+		self.dir = dir
+
+		self.ns = self.config.modules.dbnamespace
+		self.ignoreModules = [ '__init__', 'template' ]
+		self.ignoreModules.append('tac')       # moved to grufti framework in 1.0.7
+		self.ignoreModules.append('bullshitr') # moved to grufti framework in 1.0.7
+		self.moduleDir = self.dir + '/modules'
+		self.outputLock = threading.RLock()
+
+		# dynamically generated content
+		self.usageLines = []
+		self.modules = {}
+		self.loadModules()
+
+	def status(self, msg=None, level=INFO):
+		logging.log(msg=msg, level=level)
+
+	def start(self):
+		pass
+
+	def output(self, message, req):
+		pass
+
+	def botName(self):
+		return 'unknown'
+
+	"""
+	Dynamic loading of module extensions. This looks for .py files in
+	The module directory. They must be well-formed (based on template.py).
+	If there are any problems loading, it will skip them instead of crashing.
+	"""
+	def loadModules(self):
+		try: disabled = re.split('\s*[,;]\s*', self.config.modules.disabled)
+		except: disabled = []
+
+		files = os.walk(self.moduleDir).next()[2]
+		self.status('[MOD] * Reading modules from %s' % self.moduleDir, INFO)
+
+		for file in files:
+			if file.endswith('.py') is False: continue
+			modName = file[:-3]
+
+			if modName in self.ignoreModules: continue
+
+			if modName in disabled:
+				self.status('[MOD] Skipping %s because it is disabled in config' % modName, WARN)
+				continue
+
+			try:
+				module = __import__('modules.' + modName, globals(), locals(), ['match'])
+				MatchClass = getattr(module, 'match')
+				obj = MatchClass(config=self.config, ns=self.ns, dir=self.dir)
+
+				if obj.enabled is False: raise Exception, 'disabled'
+
+				if hasattr(obj, 'help') and obj.help is not None:
+					self.usageLines.append(obj.help)
+
+				self.status('[MOD] Loaded module %s' % modName, INFO)
+				self.modules[modName] = obj
+
+			except Exception, e:
+				self.status("[MOD] WARN: Couldn't load module %s: %s" % (modName, e), WARN)
+
+
+	# pre-processing filter that catches whether the bot is being addressed or not..
+	def checkAddressing(self, req):
+		nick = self.botName()
+
+		# compile regex based on current nick
+		self.reCorrection = re.compile('^\s*no,?\s*%s\s*[,:> -]+\s*(.+)' % re.escape(nick), re.I)
+		self.reAddressed = re.compile('^\s*%s\s*[,:> -]+\s*(.+)' % re.escape(nick), re.I)
+		self.reFeedback = re.compile('^\s*%s\s*\?+$' % re.escape(nick), re.I)
+
+		# correction: "no, bot, foo is bar"
+		try:
+			req.message = self.reCorrection.search(req.message).group(1)
+			req.correction = True
+			req.addressed = True
+		except: pass
+
+		# bot ping: "bot?"
+		if self.reFeedback.search(req.message):
+			req.feedback = True
+
+		# addressed
+		try:
+			req.message = self.reAddressed.search(req.message).group(1)
+			req.addressed = True
+		except: pass
+
+
+	# returns our help data as a string
+	def usage(self):
+		return '\n'.join(self.usageLines)
+
+
+	# actually process messages!
+	def processMessage(self, req):
+		if req.feedback is True:
+			self.output(message='yes?', req=req)
+			return
+
+		if req.addressed is True and req.message.lower() == 'help':
+			self.output(message=self.usage(), req=req)
+			return
+
+		for module in self.modules.values():
+			if module.requireAddressing is True and req.addressed is False:
+				continue
+
+			try: matchGroups = module.pattern.search(req.message).groups()
+			except: continue
+
+			# DON'T CHANGE THIS, IT IS DONE FOR A REASON (threadsafe)
+			kwargs = dict(req.params.items() + [('args', matchGroups), ('module', module), ('req', req)])
+
+			if self.allowThreading is True and module.thread is True:
+				t = threading.Thread(target=self.processThread, kwargs=kwargs)
+				t.start()
+			else:
+				response = module.response(**kwargs)
+				if response is not None: self.output(message=response, req=req)
+
+
+	def processThread(self, **kwargs):
+		response = kwargs['module'].response(**kwargs)
+		if response is not None:
+			self.outputLock.acquire()
+			self.output(message=response, req=kwargs['req'])
+			self.outputLock.release()
+
+
+"""
+Class to handle configuration directives. Usage is: config.module.attribute
+module maps to the headers in the configuration file. It automatically
+translates floats and integers to the appropriate type.
+"""
+class Config(object):
+	def __init__(self, file=None, section=None, opts=None):
+		if file is not None:
+			cfg = ConfigParser()
+			cfg.read(file)
+
+			for section in cfg.sections():
+				obj = Config(section=section, opts=cfg.items(section))
+				setattr(self, section, obj)
+
+		else:
+			isInt = re.compile('^[0-9]+$')
+			isFloat = re.compile('^\d+\.\d+$')
+			isTrue = re.compile('^\s*(true|yes|on|1)\s*$')
+			isFalse = re.compile('^\s*(false|no|off|0)\s*$')
+
+			for key, val in opts:
+				if isInt.search(val): val = int(val)
+				elif isFloat.search(val): val = float(val)
+				elif isTrue.search(val): val = True
+				elif isFalse.search(val): val = False
+				setattr(self, key, val)
+
+	def __getattr__(self, attr):
+		try: return getattr(self, attr)
+		except:
+			try: return getattr(self, attr.lower())
+			except: return None
+
+
+
+"""
+Standard method of daemonizing on POSIX systems
+"""
+def detach():
+	if os.name != 'posix': return False
+	if os.fork() > 0: sys.exit(0)
+	os.setsid()
+	if os.fork() > 0: sys.exit(0)
+	for fd in sys.stdout, sys.stderr: fd.flush()
+	si = file('/dev/null', 'r')
+	so = file('/dev/null', 'a+')
+	se = file('/dev/null', 'a+', 0)
+	os.dup2(si.fileno(), sys.stdin.fileno())
+	os.dup2(so.fileno(), sys.stdout.fileno())
+	os.dup2(se.fileno(), sys.stderr.fileno())
+	return True
+
+
+"""
+Entry point to set up bot and run it
+"""
+def main(argv=None):
+	# where we are being run from
+	dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+	sys.path.append(dir)
+
+	# parse commandline options
+	parser = OptionParser(version=__version__)
+	parser.add_option('-c', '--config', default=dir+'/madcow.ini', help='default: %default', metavar='FILE')
+	parser.add_option('-d', '--detach', action='store_true', default=False, help='detach when run')
+	parser.add_option('-p', '--protocol', help='force the use of this output protocol')
+	parser.add_option('-v', '--verbose', action='store_true', default=False, help='turn on verbose output')
+	parser.add_option('-D', '--debug', action='store_true', default=False, help='turn on debugging output')
+	opts, args = parser.parse_args()
+
+	if opts.debug is True:
+		logging.root.setLevel(DEBUG)
+	elif opts.verbose is True:
+		logging.root.setLevel(INFO)
+	else:
+		logging.root.setLevel(WARN)
+
+
+	# read config file
+	config = Config(file=opts.config)
+
+	# load specified protocol
+	if opts.protocol is not None: protocol = opts.protocol
+	else: protocol = config.main.module
+
+	# dynamic load of protocol handler
+	try:
+		module = __import__('protocols.' + protocol, globals(), locals(), ['ProtocolHandler'])
+		ProtocolHandler = getattr(module, 'ProtocolHandler')
+	except Exception, e:
+		logging.exception(e)
+		return 1
+
+	# daemonize if requested
+	if config.main.detach is True or opts.detach is True:
+		if detach() is True:
+			logging.shutdown()
+
+	bot = ProtocolHandler(config=config, dir=dir)
+	bot.start()
+
+	return 0
+
+
+if __name__ == '__main__': sys.exit(main())
---
+++ tags/madcow-1.0.8/protocols/irc.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+import textwrap
+from include import irclib
+import time
+import re
+import sys
+from madcow import Madcow, Request
+from include.colorlib import ColorLib
+import random
+from logging import DEBUG, INFO, WARN, ERROR, CRITICAL
+import logging
+
+# set for LOTS of verbosity
+irclib.DEBUG = 0
+
+class ProtocolHandler(Madcow):
+	def __init__(self, config=None, dir=None):
+		self.allowThreading = True
+		self.colorlib = ColorLib(type='mirc')
+
+		Madcow.__init__(self, config=config, dir=dir)
+
+		if logging.root.level <= DEBUG:
+			irclib.DEBUG = 1
+
+		self.irc = irclib.IRC()
+		self.server = self.irc.server()
+		self.events = ['welcome', 'disconnect', 'kick', 'privmsg', 'pubmsg']
+		self.channels = re.split('\s*[,;]\s*', self.config.irc.channels)
+
+	def connect(self):
+		self.status('[IRC] * Connecting to %s:%s' % (self.config.irc.host, self.config.irc.port), INFO)
+		self.server.connect(self.config.irc.host, self.config.irc.port, self.config.irc.nick)
+
+	def start(self):
+		self.connect()
+		for event in self.events:
+			self.status('[IRC] * Registering event: %s' % event, INFO)
+			self.server.add_global_handler(event, getattr(self, 'on_' + event), 0)
+
+		self.irc.process_forever()
+
+	def botName(self):
+		return self.server.get_nickname()
+
+	# welcome event triggers startup sequence
+	def on_welcome(self, server, event):
+		self.status('[IRC] * Connected', INFO)
+
+		# TODO: identify with nickserv
+
+		# become an oper
+		if self.config.irc.oper is True:
+			self.status('[IRC] * Becoming an OPER', INFO)
+			self.server.oper(self.config.irc.operUser, self.config.irc.operPass)
+
+		# join all channels
+		for channel in self.channels:
+			self.status('[IRC] * Joining: %s' % channel, INFO)
+			self.server.join(channel)
+
+
+	# when losing connection, reconnect if configured to do so, otherwise exit
+	def on_disconnect(self, server, event):
+		self.status('[IRC] * Disconnected from server', WARN)
+
+		if self.config.irc.reconnect is True:
+			if self.config.irc.reconnectWait > 0:
+				time.sleep(self.config.irc.reconnectWait)
+			self.connect()
+		else:
+			sys.exit(1)
+
+	# when kicked, rejoin channel if configured to do so
+	def on_kick(self, server, event):
+		self.status('[IRC] * Kicked from %s by %s' % (event.arguments()[0], event.target()), WARN)
+		if event.arguments()[0].lower() == server.get_nickname().lower():
+			if self.config.irc.rejoin is True:
+				if self.config.irc.rejoinWait > 0:
+					time.sleep(self.config.irc.rejoinWait)
+				server.join(event.target())
+				server.privmsg(event.target(), self.config.irc.rejoinReply)
+
+	# function to putput to IRC
+	def output(self, message, req):
+		if message is None: return
+
+		if req.colorize is True:
+			style = random.choice(ColorLib.rainbowStyles)
+			message = self.colorlib.rainbow(message, style=style)
+
+		if req.wrap is True:
+			wrap = self.config.irc.wrap
+		else:
+			wrap = 400
+
+		output = []
+		for line in message.splitlines():
+			for wrapped in textwrap.wrap(line, wrap):
+				output.append(wrapped)
+
+		for line in output:
+			self.server.privmsg(req.sendTo, line)
+
+
+	def on_privmsg(self, server, event):
+		self.status('[IRC] PRIVMSG from %s: %s' % (event.source(), event.arguments()[0]), INFO)
+		self.on_message(server, event, private=True)
+
+	def on_pubmsg(self, server, event):
+		self.status('[IRC] <%s/%s> %s' % (event.source(), event.target(), event.arguments()[0]), INFO)
+		self.on_message(server, event, private=False)
+
+	def on_message(self, server, event, private):
+		req = Request(message=event.arguments()[0])
+		req.nick = irclib.nm_to_n(event.source())
+		req.channel = event.target()
+		req.private = private
+
+		if private is True:
+			req.sendTo = req.nick
+		else:
+			req.sendTo = req.channel
+
+		self.preProcess(req)
+		self.processMessage(req)
+
+	def preProcess(self, req):
+		self.checkAddressing(req)
+
+		if req.message.startswith('^'):
+			req.message = req.message[1:]
+			req.colorize = True
+		else:
+			req.colorize = False
+
+
+
